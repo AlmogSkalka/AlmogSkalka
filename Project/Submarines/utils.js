0d779a7c280742ad2b6d08ef3e54519b7ab761cd
AlmogSkalka <73486692+AlmogSkalka@users.noreply.github.com>
2021-11-08 18:47:44 +0200

    Delete Project directory

--- Project/Submarines/utils.js
+++
@@ -1,8 +0,0 @@
-﻿function writeToDiv(divId, htmlStr, override) {
-    let d = document.getElementById(divId);
-    if (override)
-        d.innerHTML = htmlStr;
-    else
-        d.innerHTML += htmlStr;
-
-}
